76d2140182fc68f72167b8a2081947258b7263c6
Michał Morus <michalmorus@gmail.com>
2018-11-15 14:13:33 +0100

    logger

--- src/main/java/logger/Main.java
+++ src/main/java/logger/Main.java
@@ -1,20 +1,32 @@
 package logger;
 
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.logging.*;
 
 public class Main {
 
     static Logger logger = LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     public static void main(String ...args) {
+        logger.setLevel(Level.ALL);
+        logger.finer("ENTRY");
         logger.log(Level.INFO, "logger info");
         Main.exampleFunction();
+        logger.finer("RETURN");
 
+        try {
+            FileHandler h = new FileHandler("%t/logger.log", 1000, 2);
+            h.setFormatter(new SimpleFormatter());
+            logger.addHandler(h);
+            logger.log(Level.SEVERE, "test log");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void exampleFunction() {
         logger.entering("Main", "exampleFunction" );
+        System.out.println("in function");
+        logger.exiting("Main", "exampleFunction" );
     }
 }
